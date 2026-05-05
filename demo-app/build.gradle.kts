88eec856e1d954ef426228df3b397c0d7d99d554
Aleksei Grachev <gracaled79@gmail.com>
2026-05-05 17:45:39 +0500

    ADT-1372

--- demo-app/build.gradle.kts
+++ demo-app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.veonadtech.pixeltrackerdemo"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.veonadtech.pixel_tracker_android_sdk"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
